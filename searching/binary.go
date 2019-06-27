554b11c01bc2a5aed860107701a6f806aaf45bd1
manjutengale <manjutengale9@gmail.com>
2019-06-27 11:26:14 +0530

    iterative way

--- searching/binary.go
+++ searching/binary.go
@@ -2,7 +2,25 @@ package main
 
 import "fmt"
 
-func binary(arr [7]int, low int, high int, key int) int {
+func iterativebinary(arr []int, key int) int {
+	low := 0
+	high := len(arr) - 1
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if arr[mid] == key {
+			return mid
+		}
+		if arr[mid] < key {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+
+	}
+	return -1
+}
+func binary(arr []int, low int, high int, key int) int {
 	//arrsize :=
 	mid := low + (high-low)/2
 	//fmt.Println("the mid element is ", mid)
@@ -16,13 +34,14 @@ func binary(arr [7]int, low int, high int, key int) int {
 	return 0
 }
 func main() {
-
-	arr := [7]int{1, 2, 3, 4, 5, 6, 7}
-	key := 1
+	arr := []int{1, 2, 3, 4, 5, 6, 7}
+	key := 5
 	fmt.Println("calling func ")
 	arrsize := len(arr)
 	low := 0
 	high := arrsize - 1
+	positioniterative := iterativebinary(arr, key)
 	position := binary(arr, low, high, key)
 	fmt.Println("Found the array element at the position", position)
+	fmt.Println("iter Found the array element at the position", positioniterative)
 }
